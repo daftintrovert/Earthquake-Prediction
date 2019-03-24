ee2653613342066de9d9672b30c7579db020fa52
Vishal Sharma <vishalsharma.viz@gmail.com>
2019-03-24 20:08:32 +0530

    Update lanl-earthquake-prediction.py

--- lanl-earthquake-prediction.py
+++ lanl-earthquake-prediction.py
@@ -1,27 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in 
-
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
 
 import os
 print(os.listdir("../input"))
-
-# Any results you write to the current directory are saved as output.
-
-
-# In[2]:
-
 
 import matplotlib.pyplot as plt
 from tqdm import tqdm
@@ -30,33 +15,14 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_absolute_error
 
 
-# In[3]:
-
-
 train = pd.read_csv('../input/train.csv', dtype={'acoustic_data': np.int16, 'time_to_failure': np.float64})
 
-
-# In[4]:
-
-
 train.head()
-
-
-# In[5]:
-
 
 pd.options.display.precision = 15
 
 
-# In[6]:
-
-
 train.head()
-
-
-# In[7]:
-
-
 
 
 rows = 150_000
@@ -80,30 +46,17 @@ for segment in tqdm(range(segments)):
     X_train.loc[segment, 'min'] = x.min()
 
 
-# In[8]:
-
 
 X_train.head()
-
-
-# In[9]:
 
 
 scaler = StandardScaler()
 scaler.fit(X_train)
 X_train_scaled = scaler.transform(X_train)
 
-
-# In[10]:
-
-
 svm = NuSVR()
 svm.fit(X_train_scaled, y_train.values.flatten())
 y_pred = svm.predict(X_train_scaled)
-
-
-# In[11]:
-
 
 plt.figure(figsize=(6, 6))
 plt.scatter(y_train.values.flatten(), y_pred)
@@ -115,27 +68,13 @@ plt.plot([(0, 0), (20, 20)], [(0, 0), (20, 20)])
 plt.show()
 
 
-# In[12]:
-
-
 score = mean_absolute_error(y_train.values.flatten(), y_pred)
 print(f'Score: {score:0.3f}')
-
-
-# In[13]:
-
 
 submission = pd.read_csv('../input/sample_submission.csv', index_col='seg_id')
 
 
-# In[14]:
-
-
 X_test = pd.DataFrame(columns=X_train.columns, dtype=np.float64, index=submission.index)
-
-
-# In[15]:
-
 
 for seg_id in X_test.index:
     seg = pd.read_csv('../input/test/' + seg_id + '.csv')
@@ -146,27 +85,6 @@ for seg_id in X_test.index:
     X_test.loc[seg_id, 'std'] = x.std()
     X_test.loc[seg_id, 'max'] = x.max()
     X_test.loc[seg_id, 'min'] = x.min()
-
-
-# In[16]:
-
-
-
-
-
-# In[16]:
-
-
-
-
-
-# In[16]:
-
-
-
-
-
-# In[16]:
 
 
 
